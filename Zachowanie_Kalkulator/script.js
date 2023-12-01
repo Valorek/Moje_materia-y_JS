602459e4b897924ec5618027e0fdcf5708c8adf3
Valorek <145722850+Valorek@users.noreply.github.com>
2023-12-01 11:15:24 +0100

    Update script.js

--- Zachowanie_Kalkulator/script.js
+++ Zachowanie_Kalkulator/script.js
@@ -28,28 +28,28 @@ function dane(d, formId) {
             z = value.punkty;
         }
 
-        // Utwórz nowy element "option"
+        //nowy element option
         const option = document.createElement("option");
 
-        // Ustaw atrybuty dla elementu "option"
+        //atrybuty dla elementu option
         option.value = `p${key}`;
         option.textContent = `${value.pozycja} [${z}]`;
 
-        // Dodaj obsługę zdarzenia do elementu "option"
-        option.addEventListener("click", () => wywolaj(value));
+        //obsługa zdarzenia do elementu option
+        option.addEventListener("onclick", () => wywolaj(value));
 
-        // Dodaj element "option" do formularza
+        // Dodanie elementu option do formularza
         selectElement.appendChild(option);
     });
 }
 
 function wywolaj(value) {
     const wypluwacz = document.getElementById("wypluwacz");
-
-    // Ustawienie treści w elemencie "wypluwacz"
     if (typeof value.punkty === "object") {
         wypluwacz.textContent = `${value.punkty.p0}-${value.punkty.p1}`;
     } else {
         wypluwacz.textContent = value.punkty;
     }
 }
+
+//Nie mam pojęcia dlaczego wartości dodanie są undefined :( 
